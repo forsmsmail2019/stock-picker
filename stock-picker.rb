66807419fad9b56036afb3336349757b30397178
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-02-03 21:35:29 -0500

    remove puts

--- stock-picker.rb
+++ stock-picker.rb
@@ -4,31 +4,31 @@ def stock_picker(stock_prices)
   best_sell = -1
   best_sell_index = -1
   best_profit = 0
+  resul = [-1,-1]
   stock_prices.each_with_index do |stock_price, index|
+    for j in index..stock_prices.size-1
+      profit = stock_prices[j] - stock_price
+      #puts profit
 
-  for j in index..stock_prices.size-1
+      if (profit > best_profit) && (stock_prices[j] > stock_price) && (index < j)
+        #puts "sell by : #{stock_price}"
+        #puts "buy by: #{stock_prices[j]}"
+        best_profit = profit
+        best_buy_index = index
+        best_sell_index = j
+        resul[0] = best_buy_index
+        resul[1] = best_sell_index
 
+      end
+      #puts "#{index} : #{stock_price} and j : #{j}"
 
-   profit = stock_prices[j] - stock_price
-   #puts profit
-
-  if (profit > best_profit) && (stock_prices[j] > stock_price) && (index < j)
-    puts "sell by : #{stock_price}"
-    puts "buy by: #{stock_prices[j]}"
-    best_profit = profit
-    best_buy_index = j
-    best_sell_index = index
-
+    end
   end
-    #puts "#{index} : #{stock_price} and j : #{j}"
+#puts best_profit
+#puts best_buy_index
+#puts best_sell_index
 
-  end
-
-end
-puts best_profit
-puts best_buy_index
-puts best_sell_index
-
+puts "#{resul} for a profit of #{stock_prices[resul[1]]} - #{stock_prices[resul[0]]} ==  #{stock_prices[resul[1]]- stock_prices[resul[0]]} "
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
