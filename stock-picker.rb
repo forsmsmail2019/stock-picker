77bdb151872cd047169eb95470ef25ed39117502
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-02-03 21:36:51 -0500

    delete some spaces

--- stock-picker.rb
+++ stock-picker.rb
@@ -9,7 +9,6 @@ def stock_picker(stock_prices)
     for j in index..stock_prices.size-1
       profit = stock_prices[j] - stock_price
       #puts profit
-
       if (profit > best_profit) && (stock_prices[j] > stock_price) && (index < j)
         #puts "sell by : #{stock_price}"
         #puts "buy by: #{stock_prices[j]}"
@@ -18,17 +17,14 @@ def stock_picker(stock_prices)
         best_sell_index = j
         resul[0] = best_buy_index
         resul[1] = best_sell_index
-
       end
       #puts "#{index} : #{stock_price} and j : #{j}"
-
     end
   end
 #puts best_profit
 #puts best_buy_index
 #puts best_sell_index
-
-puts "#{resul} for a profit of #{stock_prices[resul[1]]} - #{stock_prices[resul[0]]} ==  #{stock_prices[resul[1]]- stock_prices[resul[0]]} "
+puts "#{resul} for a profit of #{stock_prices[resul[1]]} - #{stock_prices[resul[0]]} =  #{stock_prices[resul[1]]- stock_prices[resul[0]]} "
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
