5dcdcde6565e28d964c200803141ed5585a224f9
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-02-03 20:28:44 -0500

    add loop for each stock prices

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,11 +1,20 @@
 def stock_picker(stock_prices)
   best_buy_index = -1
+  best_buy = -1
+  best_sell = -1
   best_sell_index = -1
-  bestt_profit = 0
+  best_profit = 0
   stock_prices.each_with_index {|stock_price, index|
-  puts "#{index} : #{stock_price}"
+
+  for j in index..stock_prices.size
+
+    puts "#{index} : #{stock_price} and j : #{j}"
+
+
+  best_buy_index = index
+  end
  }
 
 end
 
-stock_picker([1,2,3,4,5])
+stock_picker([17,3,6,9,15,8,6,1,10])
